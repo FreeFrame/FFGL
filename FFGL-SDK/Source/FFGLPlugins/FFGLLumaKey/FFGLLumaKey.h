009f246619fd6f41bebb28960beccd7ae476e452
rebbur <rebbur@ee77a899-1721-0410-aef1-8f829272ceeb>
2006-12-13 10:09:50 +0000

    Added a msvc8 project folder, because a have a newer version of msvc. I added a LumaKey multiInput plugin that takes to two textures and does a simple lumakey. There is no way now to see what it does in the host of course but maybe we can add some code to the host to show how to use a multinput plugin.

---
+++ FFGL-SDK/Source/FFGLPlugins/FFGLLumaKey/FFGLLumaKey.h
@@ -0,0 +1,47 @@
+#ifndef FFGLLumaKey_H
+#define FFGLLumaKey_H
+
+#include <FFGLShader.h>
+#include "../FFGLPluginSDK.h"
+
+class FFGLLumaKey :
+public CFreeFrameGLPlugin
+{
+public:
+  FFGLLumaKey();
+  ~FFGLLumaKey() {}
+
+	///////////////////////////////////////////////////
+	// FreeFrame plugin methods
+	///////////////////////////////////////////////////
+	
+	DWORD	SetParameter(const SetParameterStruct* pParam);		
+	DWORD	GetParameter(DWORD dwIndex);					
+	DWORD	ProcessOpenGL(ProcessOpenGLStruct *pGL);
+
+	///////////////////////////////////////////////////
+	// Factory method
+	///////////////////////////////////////////////////
+
+	static DWORD __stdcall CreateInstance(void** ppInstance)
+    {
+  	  *ppInstance = new FFGLLumaKey();
+	  if (*ppInstance != NULL) return FF_SUCCESS;
+	  return FF_FAIL;
+    }
+
+protected:	
+	// Parameters
+	float m_Luma;
+	
+	int m_initResources;
+	FFGLExtensions m_extensions;
+    FFGLShader m_shader;
+	GLint m_inputTextureLocation1;
+	GLint m_inputTextureLocation2;
+	GLint m_maxCoordsLocation;
+	GLint m_LumaLocation;
+};
+
+
+#endif
