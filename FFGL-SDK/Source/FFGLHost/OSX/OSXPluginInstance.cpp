3c98ef0ce0f566694b6871081913e945aa0601c7
gabor_papp <gabor_papp@ee77a899-1721-0410-aef1-8f829272ceeb>
2009-10-16 09:53:11 +0000

    fixed osx plugin loading, added osx makefile for plugins

--- FFGL-SDK/Source/FFGLHost/OSX/OSXPluginInstance.cpp
+++ FFGL-SDK/Source/FFGLHost/OSX/OSXPluginInstance.cpp
@@ -8,10 +8,10 @@ public FFGLPluginInstance
 {
 public:
   OSXPluginInstance();
-  
+
   DWORD Load(const char *filename);
   DWORD Unload();
-  
+
   virtual ~OSXPluginInstance();
 
 protected:
@@ -36,43 +36,43 @@ DWORD OSXPluginInstance::Load(const char *fname)
     return FF_FAIL;
 
   Unload();
-  
+
   if (NSCreateObjectFileImageFromFile(fname, &m_ffImage)!=NSObjectFileImageSuccess)
     return FF_FAIL;
 
-  NSModule m_ffModule =
+  m_ffModule =
     NSLinkModule(
       m_ffImage,
       fname,
       NSLINKMODULE_OPTION_NONE);
-      
+
   if (m_ffModule==NULL)
   {
     Unload(); //to undo NSCreateObjectFileImageFromFile
     return FF_FAIL;
   }
-  
+
   NSSymbol s = NSLookupSymbolInModule(m_ffModule, "_plugMain");
   if (s==NULL)
   {
     Unload();//to undo NSLinkModule and NSCreateObjectFileImageFromFile
     return FF_FAIL;
   }
-  
-	FF_Main_FuncPtr pFreeFrameMain = (FF_Main_FuncPtr)NSAddressOfSymbol(s);
+
+  FF_Main_FuncPtr pFreeFrameMain = (FF_Main_FuncPtr)NSAddressOfSymbol(s);
 
 	if (pFreeFrameMain==NULL)
   {
     Unload(); //to undo same
     return FF_FAIL;
   }
-  
+
   m_ffPluginMain = pFreeFrameMain;
-  
+
   DWORD rval = InitPluginLibrary();
   if (rval!=FF_SUCCESS)
     return rval;
-  
+
   return FF_SUCCESS;
 }
 
@@ -83,10 +83,10 @@ DWORD OSXPluginInstance::Unload()
     FFDebugMessage("Failed to call DeInstantiateGL before Unload()");
     return FF_FAIL;
   }
-  
+
   DeinitPluginLibrary();
-  
-	if (m_ffModule != NULL)
+
+  if (m_ffModule != NULL)
   {
     if (NSUnLinkModule(m_ffModule, NSUNLINKMODULE_OPTION_NONE))
     {
